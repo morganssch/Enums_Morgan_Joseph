48b3f87ad4473efe622d2439afd65bebbf5f33a9
morganssch <coltm576@gmail.com>
2026-08-26 08:46:59 -0500

    Add project files.

---
+++ Enums_Morgan_Joseph/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Runtime.InteropServices.ComTypes;
+using System.IO;
+
+
+enum Months
+{
+    Jan =1,
+    Feb,
+    Mar,
+    Apr,
+    May,
+    Jun,
+    Jul,
+    Aug,
+    Sep,
+    Oct,
+    Nov,
+    Dec
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int myBirhtMonth = (int)Months.Jun;
+        int myBirthDay = 30;
+        int myBirthYear = 2009;
+
+        string myBirthDate = $"My birthday is {myBirhtMonth}/{myBirthDay}/{myBirthYear}";
+
+        File.WriteAllText("myFile.txt", "This is my text file!\n");
+        Console.WriteLine(File.ReadAllText("myFile.txt"));
+
+        File.AppendAllText("myFile.txt", "I added more text unc!\n");
+        Console.WriteLine(File.ReadAllText("myFile.txt"));
+
+        if(!File.Exists("newFile.txt"))
+        {
+            File.Copy("myFile.txt", "newFile.txt");
+
+        }
+        else
+        {
+            File.Replace("newFile.txt", "myFile.txt", "backupFile.txt");
+        }
+
+        File.AppendAllText("newFile.txt", myBirthDate);
+        Console.WriteLine(File.ReadAllText("newFile.txt"));
+
+    }
+}
