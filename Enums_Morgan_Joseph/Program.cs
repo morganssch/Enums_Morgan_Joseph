d70b3ab7977ab30372950e2533482f3b41f53d5c
morganssch <coltm576@gmail.com>
2026-09-01 09:19:14 -0500

    Update Program.cs

--- Enums_Morgan_Joseph/Program.cs
+++ Enums_Morgan_Joseph/Program.cs
@@ -2,7 +2,7 @@
 using System.IO;
 
 
-enum Months
+enum Months //labeled months and gave them a number
 {
     Jan =1,
     Feb,
@@ -22,30 +22,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        int myBirhtMonth = (int)Months.Jun;
-        int myBirthDay = 30;
-        int myBirthYear = 2009;
+        int myBirhtMonth = (int)Months.Jun; //made the month a number using the enum
+        int myBirthDay = 30; //made the day a number
+        int myBirthYear = 2009; //made the year a number
 
-        string myBirthDate = $"My birthday is {myBirhtMonth}/{myBirthDay}/{myBirthYear}";
+        string myBirthDate = $"My birthday is {myBirhtMonth}/{myBirthDay}/{myBirthYear}"; //made the birthday a string using the numbers
 
-        File.WriteAllText("myFile.txt", "This is my text file!\n");
-        Console.WriteLine(File.ReadAllText("myFile.txt"));
+        File.WriteAllText("myFile.txt", "This is my text file!\n"); //write a text file with the text "This is my text file!"
+        Console.WriteLine(File.ReadAllText("myFile.txt")); //read the text file and print it to the console
 
-        File.AppendAllText("myFile.txt", "I added more text unc!\n");
-        Console.WriteLine(File.ReadAllText("myFile.txt"));
+        File.AppendAllText("myFile.txt", "I added more text unc!\n"); //append more text to the text file
+        Console.WriteLine(File.ReadAllText("myFile.txt")); //read the text file and print it to the console
 
-        if(!File.Exists("newFile.txt"))
+        if (!File.Exists("newFile.txt")) //made a new file called newFile.txt if it doesn't exist
         {
-            File.Copy("myFile.txt", "newFile.txt");
+            File.Copy("myFile.txt", "newFile.txt"); //copied the myFile.txt to a new file called newFile.txt
 
         }
         else
         {
-            File.Replace("newFile.txt", "myFile.txt", "backupFile.txt");
+            File.Replace("newFile.txt", "myFile.txt", "backupFile.txt"); //replace the new file with the myFile.txt and make a backup of the new file
         }
 
-        File.AppendAllText("newFile.txt", myBirthDate);
-        Console.WriteLine(File.ReadAllText("newFile.txt"));
+        File.AppendAllText("newFile.txt", myBirthDate); //put the birthday in the new file
+        Console.WriteLine(File.ReadAllText("newFile.txt")); //wrote the new file with the birthday and printed it to the console
 
     }
 }
